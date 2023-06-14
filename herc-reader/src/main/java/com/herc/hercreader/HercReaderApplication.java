60a5c752c267fc7690e9b70c0b65f115606810ea
augustoibarrola <65631096+augustoibarrola@users.noreply.github.com>
2023-06-14 01:59:28 +0000

    Unzipped spring-boot file

---
+++ herc-reader/src/main/java/com/herc/hercreader/HercReaderApplication.java
@@ -0,0 +1,13 @@
+package com.herc.hercreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HercReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HercReaderApplication.class, args);
+	}
+
+}
---
+++ herc-reader/src/test/java/com/herc/hercreader/HercReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herc.hercreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HercReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
